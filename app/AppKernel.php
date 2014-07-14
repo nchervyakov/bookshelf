ce8c012bbf311766d08ac47f2bda6af0404e1319
Nikolay Chervyakov <nick.chervyakov@gmail.com>
2014-07-14 12:22:02 +0700

    Add AppBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new \FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Bookshelf\AppBundle\BookshelfAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Bookshelf/AppBundle/BookshelfAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bookshelf\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BookshelfAppBundle extends Bundle
+{
+}
